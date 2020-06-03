85b8ab91c60570930c68f49c84d3280437e38384
Eliseu Videira <eliseuvideira1@gmail.com>
2020-06-03 20:43:46 -0300

    Navigation between pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h1>Gatsby Project</h1>
+      <Link to="other">Other Page</Link>
+      <br />
+      <a href="https://www.youtube.com">Go Away</a>
+      <br />
+      <img
+        src="https://seeklogo.com/images/G/gatsby-logo-1A245AD37F-seeklogo.com.png"
+        alt="gatsby logo"
+        width="120px"
+      />
+    </div>
+  )
 }
--- src/pages/other.js
+++ src/pages/other.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { navigate } from "gatsby"
 
 export default function Other() {
-  return <div>Other Page</div>
+  return (
+    <div>
+      <h1>Other Page</h1>
+      <button onClick={() => navigate("/")}>To Home Page</button>
+    </div>
+  )
 }
