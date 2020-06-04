fe58e41a91c366ec50a5f98e557c3c41e8309c33
Eliseu Videira <eliseuvideira1@gmail.com>
2020-06-03 21:01:09 -0300

    Using global css and css modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,10 @@
 import React from "react"
+import styles from "./header.module.css"
 
 export default function Header({ title }) {
   return (
     <header>
-      <h1>{title}</h1>
+      <h1 className={styles.gray}>{title}</h1>
     </header>
   )
 }
