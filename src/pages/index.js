ad7bd51d73a2a74ee03bfbfb07115684e0db0a47
Eliseu Videira <eliseuvideira1@gmail.com>
2020-06-03 21:13:59 -0300

    Adding bootstrap & Making footer and header components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
     <div>
       <Header title="Gatsby Project" />
-      <Link to="other">Other Page</Link>
-      <br />
-      <a href="https://www.youtube.com">Go Away</a>
-      <br />
-      <img
-        src="https://seeklogo.com/images/G/gatsby-logo-1A245AD37F-seeklogo.com.png"
-        alt="gatsby logo"
-        width="120px"
-      />
+      <div className="container">Content</div>
+      <Footer />
     </div>
   )
 }
--- src/pages/other.js
+++ src/pages/other.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { navigate } from "gatsby"
-import Header from "../components/header"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 export default function Other() {
   return (
@@ -8,6 +9,7 @@ export default function Other() {
       <Header title="Gatsby Project" />
       <h1>Other Page</h1>
       <button onClick={() => navigate("/")}>To Home Page</button>
+      <Footer />
     </div>
   )
 }
